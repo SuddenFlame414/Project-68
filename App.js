c785fe8a636a92fbd11b028e3ebc880f371a6f9c
SuddenFlame414 <kattaparthreddy@gmail.com>
2021-09-24 11:07:31 +0530

    I added screens, Bottom navigatorand not working cause it can't compile react-navigation, I have no idea what went wrong

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer, createBottomNavigator} from 'react-navigation';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <AppContainer/>
     </View>
   );
 }
+var AppNavigator = createBottomNavigator({
+  HomeScreen:HomeScreen,
+  SummaryScreen:SummaryScreen,
+})
+const AppContainer = createAppContainer(AppNavigator);
 
 const styles = StyleSheet.create({
   container: {
